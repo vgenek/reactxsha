476a0f750b7ae364d41e1df63f671b1506749386
genek <genek@wp.pl>
2023-11-23 15:44:40 +0100

    Pierwsze zmiany w kodzie

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css'
+import sha256 from 'crypto-js/sha256';
+
+class app extends Component {
+
+  generateHash = () => {
+    var input = document.getElementById("login").value;
+    var input2 = document.getElementById("password").value;
+    var hash = sha256(input);
+    var hash2 = sha256(input2);
+    document.getElementById("reasult").innerHTML = "Zaszyfrowany login: " + hash + " Zaszyfrowane hasło" +hash2;
+  }
+
+  state = { 
+    login: '',
+    password: '',
+   } 
+  render() { 
+    return (
+      <div className="body">
+
+        <div className="container">
+        <h1>SHA256 Hash Generator</h1>
+        <input type="text" id="login" placeholder="Podaj login"></input><br/>
+        <input type="text" id="password" placeholder="Podaj hasło"></input><br/>
+        <button onClick={this.generateHash}>Zaloguj</button><br/>
+        <p id='reasult'>Zaszyfrowany tekst: </p>
+        </div>
+
+      </div>
+    );
+  }
+}
+export default app;
