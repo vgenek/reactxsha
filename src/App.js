f5d675f4f3f34dcc1db16fed046ef24c16d38ccd
Genek <vgenek@wp.pl>
2024-06-17 15:27:28 +0200

    poprawka wizualna

--- src/App.js
+++ src/App.js
@@ -9,27 +9,25 @@ class app extends Component {
     var input2 = document.getElementById("password").value;
     var hash = sha256(input);
     var hash2 = sha256(input2);
-    document.getElementById("reasult").innerHTML = "Zaszyfrowany login: " + hash + " Zaszyfrowane hasło" +hash2;
+    document.getElementById("reasult").innerHTML = "Zaszyfrowany login: " + hash + "<br> Zaszyfrowane hasło" +hash2;
   }
 
   state = { 
     login: '',
     password: '',
-   } 
-  render() { 
-    return (
-      <div className="body">
-
-        <div className="container">
-        <h1>SHA256 Hash Generator</h1>
-        <input type="text" id="login" placeholder="Podaj login"></input><br/>
-        <input type="text" id="password" placeholder="Podaj hasło"></input><br/>
-        <button onClick={this.generateHash}>Zaloguj</button><br/>
-        <p id='reasult'>Zaszyfrowany tekst: </p>
-        </div>
-
-      </div>
-    );
-  }
+   }
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                    <h1>SHA256 Hash Generator</h1>
+                    <input type="text" id="login" placeholder="Podaj login"></input><br/>
+                    <input type="text" id="password" placeholder="Podaj hasło"></input><br/>
+                    <button onClick={this.generateHash}>Zaloguj</button><br/>
+                    <p id='reasult'>Zaszyfrowany tekst: </p>
+                </div>
+            </div>
+        );
+    }
 }
 export default app;
